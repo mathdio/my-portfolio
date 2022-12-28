732cf928544d3830a0c643256c33afdc3727c4ad
Matheus Diógenes <mathdiogenes@gmail.com>
2022-12-28 17:08:26 -0300

    Ajustes de nomenclatura do React Router DOM V6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Home } />
-    </Switch>
+    <Routes>
+      <Route exact path="/" element={ <Home /> } />
+    </Routes>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+function Home() {
+  return (
+    <h1>Hello, my portfolio</h1>
+  )
+}
+
+export default Home;
