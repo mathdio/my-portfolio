e85a7c06e74c8074979f73604f4181892a65ab3d
Matheus Diógenes <mathdiogenes@gmail.com>
2022-12-29 19:59:49 -0300

    Home page em andamento

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,40 @@
+import { useEffect, useState } from 'react';
 import './Home.css';
 
 function Home() {
+  useEffect(() => {
+    document.title = 'Matheus Diógenes - Full Stack Web Dev';
+  }, []);
+
+  const [aboutInfo, setAboutInfo] = useState(false);
+  const [aboutClasses, setAboutClasses] = useState('Home-about-container');
+
+  const handleAbout = () => {
+    setAboutInfo(true);
+    setAboutClasses('Home-about-container Home-showing-container')
+  }
+
   return (
     <main className="Home-main-container">
       <div className="Home-content-container">
-      <h1>Hello, my portfolio</h1>
-
+        <div className="Home-name-title">
+          <h1>
+            Matheus Diógenes
+          </h1>
+        </div>
+        <hr className="Home-division-hr" />
+        <div className="Home-links-container">
+          <div 
+            className="Home-about"
+            onClick={ () => handleAbout() }
+          >
+            About
+          </div>
+          <div className="Home-projects">Projects</div>
+        </div>
+      </div>
+      <div className="Home-info-container">
+        {aboutInfo && <div className={ aboutClasses }></div>}
       </div>
     </main>
   )
