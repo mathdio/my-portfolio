960435d897cefc1d4b7371bcaed8b8b072fb7105
Matheus Diógenes <mathdiogenes@gmail.com>
2023-01-03 10:44:24 -0300

    feat: adição dos projetos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './Home.css';
 import githubIcon from '../images/githubIcon.svg';
 import websiteIcon from '../images/websiteIcon.svg';
+import projects from '../db/projects';
 
 function Home() {
   
@@ -65,19 +66,20 @@ function Home() {
       <div className="Home-info-container">
         <div className={ aboutClasses }></div>
         <div className={ projectsClasses }>
-          <div className="Home-individual-project-container">
+          {projects.map(({name, github, application}) => (
+            <div className="Home-individual-project-container">
             <p className="Home-project-name">
-              Recipes App Application
+              {name}
             </p>
             <div className="Home-icons-project-container">
-              <a href="https://github.com/mathdio/recipes-app-project" target="_blank" rel="noreferrer">
+              <a href={ github } target="_blank" rel="noreferrer">
                 <img
                   alt=""
                   src={ githubIcon }
                   className="Home-icon-project"
                 />
               </a>
-              <a href="https://recipes-app-project-orpin.vercel.app" target="_blank" rel="noreferrer">
+              <a href={ application } target="_blank" rel="noreferrer">
                 <img
                   alt=""
                   src={ websiteIcon }
@@ -86,6 +88,7 @@ function Home() {
               </a>
             </div>
           </div>
+          ))}
         </div>
       </div>
     </main>
